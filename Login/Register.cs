253d333c1cc5ecbb4e778e17e84294503f0a98b7
rickry <r.stoit@gmail.com>
2020-02-20 12:00:11 +0100

    Edited register

--- Login/Register.cs
+++ Login/Register.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,17 +28,34 @@ namespace Login
             string email = textBox2.Text;
             string password = textBox3.Text;
             string verepasswd = textBox4.Text;
-            int passlength = password.Count();
+            int passlength = password.Length;
 
             if (passlength > 8 || password == verepasswd)
             {
                 string postData ="email=" + email + "&password="+ password+"&name="+username;
                 var data = Helpers.webPostMethod(postData, "register");
-                MessageBox.Show("Regristration succesful, Welcome: "+username+" !");
                 Console.WriteLine(data);
-                Login newlogin = new Login();
-                newlogin.Show();
-                this.Close();
+                dynamic response = JsonConvert.DeserializeObject(data);
+                if ((bool)response.success)
+                {
+                    MessageBox.Show("Regristration succesful, Welcome: " + username + " !");
+                    Console.WriteLine(data);
+                    Login newlogin = new Login();
+                    newlogin.Show();
+                    this.Close();
+                }
+                else
+                {
+                    string errors = "";
+                    string newLine = Environment.NewLine;
+
+                    foreach (var err in response.error)
+                    {
+                        foreach (var text in err)
+                            errors += "- " +text[0] + newLine;
+                    }
+                    MessageBox.Show("Regristration failed"+ newLine + newLine + errors, "Regristration failed");
+                }
 
             }
             else if(passlength < 8)
